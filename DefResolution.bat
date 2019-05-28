f439ce971d1a3227317013f4318f301db7fa6f81
Alberto Miranda <alberto.miranda@opendeusto.es>
2019-05-28 22:05:21 +0200

    Defined Resolution script created

---
+++ DefResolution.bat
@@ -0,0 +1,38 @@
+@ECHO off
+TITLE Screen Resolution Changer v1
+
+REM ####################################
+REM    SET HERE THE RESOLUTION WANTED
+REM ####################################
+
+SET x=1920
+SET y=1080
+
+REM ####################################
+REM      USING QRES TO CHANGE RES
+REM ####################################
+
+QRes.exe /x:%x% /y:%y%
+
+CLS
+ECHO  ________________________________
+ECHO "        _____ _____   _____     "
+ECHO "      / ____|  __ \ / ____|     "
+ECHO "     | (___ | |__) | |          "
+ECHO "      \___ \|  _  /| |          "
+ECHO "      ____) | | \ \| |____      "
+ECHO "     |_____/|_|  \_\\_____|     "
+ECHO "                                "
+ECHO "   Screen Resolution Changer    "
+ECHO "________________________________"
+ECHO.
+ECHO       Script by @AlbertoMGV
+ECHO    Using QRES by Anders Kjersem
+ECHO  ________________________________
+ECHO.
+ECHO Changing Resolution to %x% x %y%...
+ECHO Done!!!
+ECHO.
+ECHO If you want another resolution edit the script!
+ECHO.
+timeout 10
